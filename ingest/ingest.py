f12daa141a5cb747f8fa410c8d18a332b2cc6073
Jonathan <jonathanu064@yahoo.com>
2026-05-14 15:28:27 -0500

    Changed df schemas to manual

--- ingest/ingest.py
+++ ingest/ingest.py
@@ -24,24 +24,29 @@ BRONZE = ROOT / "bronze"
 SILVER = CUR / "silver"
 
 #Read files into df
-movies_df = spark.read.csv(str(BRONZE / "movies_metadata.csv"), header=True,inferSchema=True)
-# movies_df.show()
+movie_schema = """id INT,imdb_id INT,title STRING,original_title STRING,overview STRING,tagline STRING,
+release_date DATE,runtime INT,budget DECIMAL(15,2),revenue DECIMAL(15,2),popularity DECIMAL(6,2),
+vote_average DECIMAL(5,2),vote_count INT,genres STRING,production_companies STRING,production_countries STRING,
+spoken_languages STRING,belongs_to_collection STRING,homepage STRING,poster_path STRING,backdrop_path STRING,
+status STRING,adult BOOLEAN,video BOOLEAN"""
+movies_df = spark.read.csv(str(BRONZE / "movies_metadata.csv"), header=True,schema=movie_schema)
 
-credits_df = spark.read.csv(str(BRONZE / "credits.csv"), header=True,inferSchema=True)
-# credits_df.show()
+credits_schema = "id INT, cast STRING, crew STRING"
+credits_df = spark.read.csv(str(BRONZE / "credits.csv"), header=True,schema=credits_schema)
 
-keywords_df = spark.read.csv(str(BRONZE / "keywords.csv"), header=True,inferSchema=True)
-# keywords_df.show()
+keywords_schema = "id INT,keywords STRING"
+keywords_df = spark.read.csv(str(BRONZE / "keywords.csv"), header=True,schema=keywords_schema)
 
-ratings_df = spark.read.csv(str(BRONZE / "ratings_small.csv"), header=True,inferSchema=True)
-# ratings_df.show()
+ratings_schema = "userId INT, movieId INT,rating DECIMAL(4,1),timestamp STRING"
+ratings_df = spark.read.csv(str(BRONZE / "ratings_small.csv"), header=True,schema=ratings_schema)
+
 
 #Dropped all rows missing essential columns, weaviate can handle other nulls
 movies_df1 = movies_df.dropna(subset=["id", "imdb_id", "overview","release_date","adult"]) \
         .dropna(subset=["title", "original_title"], how="all")\
 
 credits_df1 = credits_df.dropna(subset=["id"]) \
-        .dropna(subset=["cast","crew"], how="all").show()
+        .dropna(subset=["cast","crew"], how="all")
 
 keywords_df1 = keywords_df.dropna(subset=["id","keywords"])
 
